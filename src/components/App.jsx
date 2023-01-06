0299a600618dc044d411eb7d6837bbffc460d595
bzhezinsyi <bzhezinskyiillia@gmail.com>
2023-01-06 14:08:58 +0200

    updata

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,51 @@
-import { getTrending, getSearchMovies } from 'services/themoviedb.services';
+import {
+  getTrending,
+  getSearchMovies,
+  getMoviesDetails,
+  getMoviesCredits,
+  getMoviesReviews,
+} from 'services/themoviedb.services';
 
-const onClick = console.log(getTrending());
+// const onClick = console.log(getTrending());
 
 export default function App() {
   return (
     <div>
-      <button onClick={getSearchMovies}>+</button>
+      <button
+        onClick={() => {
+          getTrending();
+        }}
+      >
+        +
+      </button>
+      <button
+        onClick={() => {
+          getSearchMovies({ query: 'lion', page: 2 });
+        }}
+      >
+        +
+      </button>
+      <button
+        onClick={() => {
+          getMoviesDetails({ movieId: 76600 });
+        }}
+      >
+        +
+      </button>
+      <button
+        onClick={() => {
+          getMoviesCredits({ movieId: 76600 });
+        }}
+      >
+        +
+      </button>
+      <button
+        onClick={() => {
+          getMoviesReviews({ movieId: 76600 });
+        }}
+      >
+        +
+      </button>
     </div>
   );
 }
--- src/services/themoviedb.services.js
+++ src/services/themoviedb.services.js
@@ -9,10 +9,28 @@ export const getTrending = async () => {
   const { data } = await themoviedbApi.get('/trending/movie/day', {
     params: { api_key: keyApi },
   });
-  return data;
+  return console.log(data);
 };
-export const getSearchMovies = async ({ params }) => {
+export const getSearchMovies = async ({ query, page }) => {
   const { data } = await themoviedbApi.get('/search/movie', {
+    params: { api_key: keyApi, query, page },
+  });
+  return console.log(data);
+};
+export const getMoviesDetails = async ({ movieId }) => {
+  const { data } = await themoviedbApi.get(`/movie/${movieId}`, {
+    params: { api_key: keyApi },
+  });
+  return console.log(data);
+};
+export const getMoviesCredits = async ({ movieId }) => {
+  const { data } = await themoviedbApi.get(`/movie/${movieId}/credits`, {
+    params: { api_key: keyApi },
+  });
+  return console.log(data);
+};
+export const getMoviesReviews = async ({ movieId }) => {
+  const { data } = await themoviedbApi.get(`/movie/${movieId}/reviews`, {
     params: { api_key: keyApi },
   });
   return console.log(data);
